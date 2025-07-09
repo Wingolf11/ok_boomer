35c5ff8c8abb05f3ddfc101be8550ecfafc10026
Wingolf11 <juanesquiroz11@gmail.com>
2025-07-09 18:18:03 +0200

    style btn

--- index.php
+++ index.php
@@ -9,21 +9,29 @@
     </head>
     <body>
         <header>
-            <nav>
+            <nav class= nav_btns>
                 <ul>
-                    <li><a href="login_v1.html" title="S'identifier">S'identifier</a></li>
-                    <li><a href="signup.html" title="Créer un utilisateur">Créer un utilisateur</a></li>
+                    <li>
+                        <a href="login.php" title="S'identifier" class="button">
+                            <div class="button2">S'identifier</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="signup.php" title="Créer un utilisateur" class="button btn-black">
+                            <div class="button2">Créer un utilisateur</div>
+                        </a>
+                    </li>
                 </ul>
             </nav>
-            <div id="header_img">
+            <div class="header_img">
                 <img src="uploads/header_img.jpg" alt="Header Image">
             </div>
-            <div id="welcome">
+            <div class="welcome">
                 <h1>Ok Boomer!</h1> 
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero, quasi, quibusdam autem nemo vel expedita minus voluptatibus, enim laboriosam optio debitis nesciunt magnam atque nam ipsam. Ad sint quod dolor!</p>
             </div>
         </header>
-        <section id="list_art">
+        <section class="list_art">
             <?php include 'articles.php'; ?>
         </section>
         <footer>
