f5e5345074453fa0d1608669c66d0200164ae9aa
Wingolf11 <juanesquiroz11@gmail.com>
2025-07-10 13:58:03 +0200

    Corrected version of display

--- display_art.php
+++ display_art.php
@@ -1,41 +1,32 @@
 <?php
+session_start();
 require 'config/data_b.php';
 
-// Execute the query
-$myquery = $conn->prepare("SELECT * FROM articles");
+$myquery = $conn->prepare("
+    SELECT articles.*, users.login 
+    FROM articles 
+    JOIN users ON articles.id_user = users.id_user
+");
+
 $myquery->execute();
 $result = $myquery->get_result();
 
-// Check for query failure
 if (!$result) {
     die('Erreur lors de la requête : ' . $conn->error);
 }
 
-// Display the table
-echo "
-<table class='table' border='1' cellpadding='10' cellspacing='0'>
-    <tr>
-        <th>Titre</th>
-        <th>Date</th>
-        <th>Photo</th>
-        <th>Texte</th>
-        <th>ID Utilisateur</th>
-    </tr>";
-
-// Loop through the results
+echo "<table class='table' cellpadding='5' cellspacing='0'>";
 while ($row = $result->fetch_assoc()) {
     echo "<tr>
+        <td><img src='" . htmlspecialchars($row['photo']) . "' alt='Photo' width='200'></td>
         <td>" . htmlspecialchars($row['titre']) . "</td>
-        <td>" . htmlspecialchars($row['date']) . "</td>
-        <td><img src='" . htmlspecialchars($row['photo']) . "' alt='Photo' width='100'></td>
         <td>" . nl2br(htmlspecialchars($row['texte'])) . "</td>
-        <td>" . htmlspecialchars($row['id_user']) . "</td>
+        <td>" . htmlspecialchars($row['date']) . "</td>
+        <td>" . htmlspecialchars($row['login']) . "</td>
     </tr>";
 }
-
 echo "</table>";
 
-// Close the statement and connection
 $myquery->close();
 $conn->close();
 ?>
