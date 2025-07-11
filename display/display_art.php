1c46568e775f0a7a755f03d9b4181a55e461d798
Wingolf11 <juanesquiroz11@gmail.com>
2025-07-11 06:43:10 +0200

    style done

--- display/display_art.php
+++ display/display_art.php
@@ -19,18 +19,24 @@ if (!$result) {
 }
 
 // Display results
-echo "<table class='table' cellpadding='5' cellspacing='10'>";
+echo "<table class='table'>";
 while ($row = $result->fetch_assoc()) {
-    echo "<tr>
-        <td id='photo'><img src='" . htmlspecialchars($row['photo']) . "' alt='Photo' width='200'></td>
-        <td id='titre'>" . htmlspecialchars($row['titre']) . "</td>
-        <td id='texte'>" . nl2br(htmlspecialchars($row['texte'])) . "</td>
-        <td id='date'>" . htmlspecialchars($row['date']) . "</td>
-        <td id='login'>" . htmlspecialchars($row['login']) . "</td>";
-
-    // Check role-based delete permission
+    echo "<tr><td colspan='100%'>
+        <div class='article-card'>
+            <div class='card-photo'>
+                <img src='" . htmlspecialchars($row['photo']) . "' alt='Photo'>
+            </div>
+            <div class='card-content'>
+                <div class='card-header'>
+                    <div class='card-title'>" . htmlspecialchars($row['titre']) . "</div>
+                    <div class='card-meta'>
+                        <div class='card-date'>" . htmlspecialchars($row['date']) . "</div>
+                        <div class='card-author'>by " . htmlspecialchars($row['login']) . "</div>                
+                    </div>
+                </div>
+                <div class='card-text'>" . nl2br(htmlspecialchars($row['texte'])) . "</div>";
+    // Delete permission
     $canDelete = false;
-
     if ($sessionUserRole === 'superadmin' || $sessionUserRole === 'admin') {
         $canDelete = true;
     } elseif ($sessionUserRole === 'editeur' && $sessionUserId == $row['id_user']) {
@@ -38,19 +44,18 @@ while ($row = $result->fetch_assoc()) {
     }
 
     if ($canDelete) {
-        echo "<td id='delete_btn'>
+        echo "<div class='card-delete'>
                 <form method='post' action='handlers/delete_article.php' onsubmit='return confirm(\"Supprimer cet article ?\");'>
                     <input type='hidden' name='id_article' value='" . $row['id_article'] . "'>
-                    <button class='delete_button' type='submit' class='delete-btn'>Supprimer</button>
+                    <button class='delete_button' type='submit'>Supprimer</button>
                 </form>
-              </td>";
-    } else {
-        echo "<td></td>";
+              </div>";
     }
 
-    echo "</tr>";
+    echo "</div></div></td></tr>";
 }
 echo "</table>";
+
 
 $myquery->close();
 $conn->close();
